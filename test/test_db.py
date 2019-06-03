a5a19e6071a84353e54083dcccb1831014e865fb
ansable <anchous12@gmail.com>
2019-06-03 12:35:54 +0200

    ops

--- test/test_db.py
+++ test/test_db.py
@@ -801,7 +801,7 @@ class _TestQuery(object):
             self.assertEqual(v.SQL(), sql)
             self.assertEqual(v.rows(), rows)
         # Assert Database.link() permanent relations.
-        v = self.db.persons.search(fields=["name", "gender.name"]), sort=["persons.name"])
+        v = self.db.persons.search(fields=["name", "gender.name"], sort=["persons.name"])
         v.aliases["gender.name"] = "gender"
         self.db.link("persons", "gender", "gender", "id", join=db.LEFT)
         self.assertEqual(v.SQL(),
